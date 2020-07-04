4876f3941b5d8b6e02da1f5a75ebe09d0500a489
csangharsha <sangharsha.chaulagain@gmail.com>
2020-07-04 11:31:57 +0545

    turn off swagger for prod

--- web-api/src/main/java/com/csangharsha/sf_movies/conf/SwaggerConfig.java
+++ web-api/src/main/java/com/csangharsha/sf_movies/conf/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.csangharsha.sf_movies.conf;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -15,6 +16,7 @@ import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 @EnableSwagger2
+@Profile("dev")
 public class SwaggerConfig {
 
     @Value("${app.basePackage}")
