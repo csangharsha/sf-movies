3d6ac0ce51cdf6dd482f2818c27ecd33e6b89960
csangharsha <sangharsha.chaulagain@gmail.com>
2020-07-07 15:16:27 +0545

    add debounce

--- web-app/src/component/SearchBarComponent/SearchBox/index.js
+++ web-app/src/component/SearchBarComponent/SearchBox/index.js
@@ -1,21 +1,32 @@
 import React, {Component} from 'react';
+import debounce from 'lodash.debounce';
 
 import './styles.css';
 
 class SearchBox extends Component {
 
+    debounceEvent(...args) {
+        this.debouncedEvent = debounce(...args);
+        return e => {
+            e.persist();
+            return this.debouncedEvent(e);
+        }
+    }
+
+    handleChange = (event) => this.props.onValueChanged(event);
+
     render() {
-        let { searchKeyword } = this.props;
+        let {searchKeyword} = this.props;
 
         return (
             <div className={'searchbox'}>
-               <input
-                   className={'searchbox__input'}
-                   autoFocus={true}
-                   placeholder={'Search Movie'}
-                   value={searchKeyword}
-                   onChange={(event) => this.props.onValueChanged(event)}
-               />
+                <input
+                    className={'searchbox__input'}
+                    autoFocus={true}
+                    placeholder={'Search Movie'}
+                    value={searchKeyword}
+                    onChange={this.debounceEvent(this.handleChange, 500)}
+                />
             </div>
         );
     }
